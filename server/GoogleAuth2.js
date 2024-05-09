943c6d5eb0dba073e43114a27616db377732ba1f
MMBAGA JOHNSON NATHANIEL <129178005+KaReeeeeeeeEM@users.noreply.github.com>
2024-05-09 08:56:09 +0300

    Update GoogleAuth2.js

--- server/GoogleAuth2.js
+++ server/GoogleAuth2.js
@@ -7,7 +7,11 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:5000/auth/google/callback', // Replace with your callback URL
+  callbackURL: 'https://joka-movies.onrender.com:5000/auth/google/callback'                    
+                      
+                      
+                      
+  , // Replace with your callback URL
 },
 async (accessToken, refreshToken, profile, done) => {
   try {
